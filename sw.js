085c603be8db199f7f52fcf35ba98130c9f230f1
jwkinc <jwkincaid82@gmail.com>
2026-06-26 10:13:59 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'itravel54-no-cache-verified-20260626';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request, {cache: 'no-store'}).catch(() => fetch(event.request)));
+});
